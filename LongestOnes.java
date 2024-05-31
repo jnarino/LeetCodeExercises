60ee79da1dfdbecd37a89ceab8cbe44805f28279
juandata <jnarino@eafit.edu.co>
2024-05-31 11:13:33 -0400

    chore:solved new problems

--- LongestOnes.java
+++ LongestOnes.java
@@ -5,13 +5,27 @@ public class LongestOnes {
         int[] nums = { 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0 };
         int k = 2;
         System.out.println(longestOnes(nums, k));
-
     }
 
     public static int longestOnes(int[] nums, int k) {
+        int count = 0;
+        int ans = 0;
+        int left = 0;
 
-        return 0;
+        for (int righ = 0; righ < nums.length; righ++) {
+            if (nums[righ] == 0) {
+                count++;
+            }
 
+            while (count > k) {
+                if (nums[left] == 0) {
+                    count -= 1;
+                }
+                left++;
+            }
+            ans = Math.max(ans, righ - left + 1);
+
+        }
+        return ans;
     }
-
 }
